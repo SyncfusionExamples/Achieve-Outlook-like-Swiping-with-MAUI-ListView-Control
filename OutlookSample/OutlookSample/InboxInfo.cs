5de3ae5a19b698cd1af588b34ce9e381c660d2f9
snehakPV <sneha.sf4154@gmail.com>
2024-08-27 14:52:55 +0530

    Updated the sample

--- OutlookSample/OutlookSample/InboxInfo.cs
+++ OutlookSample/OutlookSample/InboxInfo.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +7,15 @@ namespace OutlookSample
 {
     public class InboxInfo
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public string ProfileName { get; set; }
+        public string? ProfileName { get; set; }
 
-        public string Subject { get; set; }
+        public string? Subject { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        public ImageSource Image { get; set; }
+        public ImageSource? Image { get; set; }
 
         public bool? IsAttached { get; set; }
 
